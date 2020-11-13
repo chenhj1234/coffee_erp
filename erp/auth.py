e6d939946691868e0b4eec6d54c34c16e610f966
holmas <holmaschen@qnap.com>
2020-11-13 19:38:17 +0800

    - base

---
+++ erp/auth.py
@@ -0,0 +1,16 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    return render_template('register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    return render_template('login.html')
